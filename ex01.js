bb0e2e955698ee394553b41005b0aef20a6796cf
Henrique Reis <henriquereiss2101@gmail.com>
2023-09-29 13:33:04 -0300

    Exercicio 1finalizado

--- ex01.js
+++ ex01.js
@@ -1,7 +1,7 @@
 const lista = [10,12,15,35,40]
 
-function maioresQueMedia(...lista){ //função que devolve os valores que são maiores que a média
-    const somaLista = lista.reduce((acumulador,atual) => acumulador + atual, 0)
+function maioresQueMedia(funcaoSoma, ...lista){ //função que devolve os valores que são maiores que a média, utilizando como parâmetro a função somaValoresLista
+    const somaLista = funcaoSoma(...lista)
     const mediaLista = somaLista/lista.length
     
     const resultado = lista.filter(numero => numero > mediaLista)
@@ -21,6 +21,17 @@ function menorValor(...lista){ //função que encontra o menor valor da lista
     console.log(menor)
 }
 
-//maioresQueMedia(...lista)
+function somaValoresLista(...lista){ //função que calcula a soma dos valores da lista
+    const somaLista = lista.reduce((acumulador,atual) => acumulador + atual, 0)
+    return somaLista
+}
 
+function maioresQueVinte(...lista){ //função que seleciona os números da lista maiores que 20
+    const resultado = lista.filter(numero => numero > 20)
+    console.log(resultado)
+}
+
+maioresQueMedia(somaValoresLista, ...lista)
 menorValor(...lista)
+console.log(somaValoresLista(...lista))
+maioresQueVinte(...lista)
